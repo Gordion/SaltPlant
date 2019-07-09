d2b63bdef652b4df6a276669f7747b7f1b55a944
Gordion <darthgordion@gmail.com>
2019-07-09 21:29:42 +0300

    Added menu

--- script.js
+++ script.js
@@ -4,6 +4,20 @@ $(document).ready(function(){
 
   var totalPrice = price * count;
 
+  var url = window.location.pathname;
+  var filename = url.substring(url.lastIndexOf('/')+1);
+
+  setCurrentPage();
+
+  function setCurrentPage() {
+    $(".menu-list li").each(function (i, el) {
+      var menuItemLink = $(el).find('a').attr('href');
+      if (filename === menuItemLink) {
+        $(el).addClass("active");
+      }
+    });
+  }
+
   function plus() {
     count += 1;
     $(".amount").text(count);
@@ -24,6 +38,21 @@ $(document).ready(function(){
     totalPrice = price * count;
     $(".buy-holder-total-price-result").text(totalPrice);
   }
+
+  function menuClick() {
+    $(".menu-main").addClass("menu-opened");
+    $(".overlay").addClass("menu-opened");
+  }
+
+  function closeMenuClick() {
+    $(".menu-main").removeClass("menu-opened");
+    $(".overlay").removeClass("menu-opened");
+  }
+
+  function getWindowLocation() {
+    console.log(window.location.href);
+  }
+
   // function cost() {
   //  var price = 200;
   //  var total = price *
@@ -39,7 +68,16 @@ $(document).ready(function(){
     calcTotalPrice();
   });
 
+  $(".menu-exc").on('click', function(e) {
+    menuClick();
+  });
 
+  $(".menu-cross").on('click', function(e) {
+    closeMenuClick();
+  });
 
+  $(".overlay").on('click', function(e) {
+    closeMenuClick();
+  });
 
 });
